3ab7d097aab66d3c4872aaa7d35ad50e19592639
Rocky O'Connell <john.oconnell@wec.net>
2025-12-14 00:25:29 -0500

    add lazy.nvim bootstrap

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
--- sanity
+-- =====================================================
+-- Baseline options
+-- =====================================================
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.expandtab = true
@@ -7,4 +9,27 @@ vim.opt.tabstop = 2
 vim.opt.termguicolors = true
 
 vim.g.mapleader = " "
+
+-- =====================================================
+-- lazy.nvim bootstrap
+-- =====================================================
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- =====================================================
+-- lazy.nvim setup (no plugins yet)
+-- =====================================================
+require("lazy").setup({})
 
